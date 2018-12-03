95549b6f5a63d4aaaa7d506ae13dd2efb139d33a
yubei <1002726595@qq.com>
2018-12-03 21:08:23 +0800

    修正sql语句

--- posts/getid.php
+++ posts/getid.php
@@ -4,23 +4,19 @@
 
     $type = $_GET['type'];
     if(strcmp($type,"idcard")==0){
-        $sheet = "lost_found_idcard";
+        $sql = "select id from lost_found_idcard";
     }elseif(strcmp($type,"lost")==0){
-        $sheet = "lost_found";
+        $sql = "select id from lost_found";
     }else{
         echo makeErrJson(40001,"illegal parameter");
         die();
     }
-    $sql = "select id from :sheet";
     $query = $db->prepare($sql);
-    $query->bindValue(':sheet',$sheet,PDO::PARAM_STR);
     $res = $query->execute();
     $count = $query->rowCount();
     $arr = array();
-    //这里下面还没修好
     for($i=0;$i<$count;$i++){
-        mysqli_data_seek($res,$i);
-        $row = $res->fetch_array();
+        $row = $query->fetch(PDO::FETCH_ASSOC);
         $arr[$i] = array(
             "id" => $row['id']
         );
